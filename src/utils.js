9567a16bb215274f2eacfc9068778fb88e582f3d
deepak sisodiya <deepak.sisodiya@courgetteconcepts.com>
2016-10-04 13:45:58 +0530

    utils added

---
+++ src/utils.js
@@ -0,0 +1,33 @@
+/**
+ * Created by deepaksisodiya on 04/10/16.
+ */
+
+export default {
+  username: {
+    rejex: /^(?=.*[A-Z])[0-9a-zA-Z]+$/,
+    error: 'Invalid Username'
+  },
+  email: {
+    rejex: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm,
+    error: 'Invalid Email'
+  },
+  password: {
+    rejex: /^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_]{5,10}$/,
+    error: 'Invalid Password'
+  }
+}
+
+// username validation
+// 1. can contain 0-9 and a to z
+// 2. must contain one uppercase letter
+
+// email validation
+
+/*
+validation password
+password can contain digits
+password can contain character lower or upper case
+password must have length between 5 to 10
+password must contain at least on uppercase letter
+password must contain at least on number
+password can contain _ (underscore)*/
